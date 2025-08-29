1e6f6f8436ec8f81917cd0deda07887a6fc01ace
Rayyan Aamir <aamir.rayyan27@gmail.com>
2025-08-29 11:55:24 +0500

    Update Q1.cpp

--- Lab Task 2/Q1.cpp
+++ Lab Task 2/Q1.cpp
@@ -8,24 +8,40 @@ Implement a program that dynamically allocates a 1D array of integers.
 #include <iostream>
 using namespace std;
 
-void updateValues(int *&arr, int size){
-    for(int i = 0; i < size; i++){
-        cout << "Enter the value to be stored at index " << i << ": ";
-        cin >> arr[i];
+void updateValues(int *arr, int size){
+    int pos;
+    while(true){
+        cout << "\nEnter the index: ";
+        cin >> pos;
+        
+        if(pos < 0 || pos >= size){
+            cout << "Out of bounds!" << endl;
+        }
+        else break;
     }
+    
+    cout << "Enter the value to be stored at index " << pos << ": ";
+    cin >> arr[pos];
 }
 
 void printArray(int *arr, int size){
-    cout << "Printing the array:" << endl;
+    cout << "\nPrinting the array:" << endl;
     for(int i = 0; i < size; i++){
-        cout << arr[i] << " ";
+        cout << "Index " << i << ": " << arr[i] << endl;
     }
 }
 
 int main() {
     int size;
-    cout << "Enter the size of the array: ";
-    cin >> size;
+    while(true){
+        cout << "Enter the size of the array: ";
+        cin >> size;
+        
+        if(size <= 0){
+            cout << "Invalid size!" << endl;
+        }
+        else break;
+    }
     
     int *arr = new int[size];
     for(int i = 0; i < size; i++)
@@ -33,6 +49,8 @@ int main() {
         
     updateValues(arr, size);
     printArray(arr, size);
+
+    delete[] arr;
 
     return 0;
 }
